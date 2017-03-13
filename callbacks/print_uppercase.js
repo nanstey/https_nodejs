d15aadd914a31997928e1d82e5fe9692bc4aeb04
Noel Anstey <anstey.ng@gmail.com>
2017-03-13 18:43:44 +0000

    print uppercase

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,15 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+  var upper = html.toUpperCase();
+  console.log(upper);
+
+}
+
+getHTML(requestOptions, printUpperCase);
+
