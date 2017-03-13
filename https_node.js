2794c538618b898378e22f5cff8358ae2a26440f
Noel Anstey <anstey.ng@gmail.com>
2017-03-13 18:32:02 +0000

    step 4

--- https_node.js
+++ https_node.js
@@ -93,4 +93,38 @@ var requestOptions = {
   path: '/http-examples/step3.html'
 };
 
-getAndPrintHTML( requestOptions );
+// getAndPrintHTML( requestOptions );
+
+function getHTML (options, callback) {
+
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    var buffer = "";
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      buffer += data;
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      callback(buffer);
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
